162b50e280ec358a96d57952da497e440a4396f9
Artem <atlas.monker@gmail.com>
2023-09-16 18:38:30 +0300

    Change  health_check

--- app/main.py
+++ app/main.py
@@ -2,14 +2,13 @@ import uvicorn
 from fastapi import FastAPI
 
 from app.conf.config import FastAPIConfig
-from app.routes import health_check
+from app.routes import health
 
 app = FastAPI()
 
 conf = FastAPIConfig()
 
-app.include_router(health_check.route, tags=["health_check"])
-
+app.include_router(health.route)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host=conf.host, port=conf.port, reload=conf.reload)
