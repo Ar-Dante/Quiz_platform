52921a886fbb6f72a104f89125c7a3d006b565b2
Artem <atlas.monker@gmail.com>
2023-09-20 20:03:48 +0300

    Change config, test and connection to redis

--- app/conf/config.py
+++ app/conf/config.py
@@ -4,11 +4,14 @@ from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
 
 load_dotenv()
-sqlalchemy_database_url = os.getenv("SQLALCHEMY_DATABASE_URL")
-redis_url = os.getenv("REDIS_URL")
 
 
 class FastAPIConfig(BaseSettings):
     host: str = os.getenv("MAIN_HOST")
     port: int = os.getenv("MAIN_PORT")
     reload: bool = os.getenv("RELOAD")
+    sqlalchemy_database_url: str = os.getenv("SQLALCHEMY_DATABASE_URL")
+    redis_url: str = os.getenv("REDIS_URL")
+
+
+conf = FastAPIConfig()
--- app/db/db.py
+++ app/db/db.py
@@ -1,25 +1,17 @@
 import aioredis
-from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.ext.asyncio import create_async_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from app.conf.config import sqlalchemy_database_url, redis_url
+from app.conf.config import conf
 
-SQLALCHEMY_DATABASE_URL = sqlalchemy_database_url
+engine = create_async_engine(conf.sqlalchemy_database_url, echo=True)
 
-engine = create_async_engine(sqlalchemy_database_url, echo=True)
-Base = declarative_base()
 async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 
-async def create_redis_pool():
-    redis_pool = await aioredis.from_url(redis_url)
-    return redis_pool
-
-
-async def get_redis(redis_pool=Depends(create_redis_pool)):
+async def get_redis():
+    redis_pool = await aioredis.from_url(conf.redis_url)
     return redis_pool
 
 
