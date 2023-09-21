2ac867cbc24831dbcdef8e416c842dfea69f3e64
Artem <atlas.monker@gmail.com>
2023-09-21 16:53:37 +0300

    Change healthcheck and coonection to redis

--- app/routes/health.py
+++ app/routes/health.py
@@ -26,7 +26,7 @@ async def check_db_connection(db: AsyncSession = Depends(get_db)):
                 status_code=500, detail="Database is not configured correctly"
             )
         logging.info("Connection to PostgreSQL was successful")
-        return {"message": "Welcome to FastAPI!"}
+        return {"status_code": 200, "detail": "ok", "result": "working"}
     except Exception as e:
         print(e)
         logging.error("Some problems with connection to PostgreSQL")
@@ -36,9 +36,9 @@ async def check_db_connection(db: AsyncSession = Depends(get_db)):
 @route.get("/check_redis")
 async def check_redis_connection(redis: aioredis.Redis = Depends(get_redis)):
     try:
-        result = await redis.ping()
+        await redis.ping()
         logging.info("Connection to Redis was successful")
-        return {"message": "Redis connection is OK", "ping_result": result}
+        return {"status_code": 200, "detail": "ok", "result": "working"}
 
     except Exception as e:
         logging.error("Some problems with connection to Redis")
