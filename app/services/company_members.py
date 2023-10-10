97c546d3b0f5915be86f2a88ec56e7484eafb1fc
Artem <atlas.monker@gmail.com>
2023-10-10 11:57:08 +0300

    add admin validation

--- app/services/company_members.py
+++ app/services/company_members.py
@@ -60,7 +60,9 @@ class CompanyMembersService:
                         company: dict,
                         current_user: int
                         ):
-        await self.validate_access_for_admins(current_user, company.owner_id, company.id, user_id)
+        await self.validate_access_for_admins(current_user, company.owner_id, user_id)
+        if await self.get_member(user_id, company.id) is None:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=ERROR_MEMBER_NOT_FOUND)
         return await self.comp_memb_repo.update_by_filter({
             "user_id": user_id,
             "company_id": company.id
@@ -71,7 +73,12 @@ class CompanyMembersService:
                            company: dict,
                            current_user: int
                            ):
-        await self.validate_access_for_admins(current_user, company.owner_id, company.id, user_id)
+        await self.validate_access_for_admins(current_user, company.owner_id, user_id)
+        member = await self.get_member(user_id, company.id)
+        if member is None:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=ERROR_MEMBER_NOT_FOUND)
+        if not member.is_admin:
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=ERROR_MEMBER_NOT_ADMIN)
         return await self.comp_memb_repo.update_by_filter({
             "user_id": user_id,
             "company_id": company.id
@@ -80,13 +87,8 @@ class CompanyMembersService:
     async def get_company_admins(self, company_id: int, limit: int, offset: int):
         return await self.comp_memb_repo.filter_by(limit, offset, {"company_id": company_id, "is_admin": True})
 
-    async def validate_access_for_admins(self, current_user: int, company_owner: int, company_id: int, user_id: int):
+    async def validate_access_for_admins(self, current_user: int, company_owner: int, user_id: int):
         if current_user != company_owner:
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=ERROR_ACCESS)
         if user_id == current_user:
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=ERROR_MEMBER_OWNER_ADMIN)
-        member = await self.get_member(user_id, company_id)
-        if member is None:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=ERROR_MEMBER_NOT_FOUND)
-        if not member.is_admin:
-            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=ERROR_MEMBER_NOT_ADMIN)
