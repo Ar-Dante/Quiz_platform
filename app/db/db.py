b92bdb111f9e0b1a70b4e02af507631af0e3436e
Artem <atlas.monker@gmail.com>
2023-10-17 18:05:01 +0300

    change connect to db

--- app/db/db.py
+++ app/db/db.py
@@ -5,7 +5,8 @@ from sqlalchemy.orm import sessionmaker
 
 from app.conf.config import conf
 
-engine = create_async_engine(conf.sqlalchemy_database_url_prod, echo=True)
+SQL_ALCHEMY_URL_PROD = f"postgresql+asyncpg://{conf.db_user_prod}:{conf.db_password_prod}@{conf.db_endpoint_prod}:{conf.db_port_prod}"
+engine = create_async_engine(SQL_ALCHEMY_URL_PROD, echo=True)
 
 async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
